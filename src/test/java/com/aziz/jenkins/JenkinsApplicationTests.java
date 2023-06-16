5b0d85dd816d47137ab9be4acfc7a1cbcf3eedcb
Abdulazezms <abdulaziizmohammeds@gmail.com>
2023-06-16 23:34:19 +0300

    first commit

---
+++ src/test/java/com/aziz/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
